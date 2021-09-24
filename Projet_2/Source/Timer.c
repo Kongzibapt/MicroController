981ec712842a2c9604fb35a174b95dc8cca72294
ehr <ehr@etud.insa-toulouse.fr>
2021-09-24 15:12:12 +0200

    LED Clignotante OK

--- Projet_2/Source/Timer.c
+++ Projet_2/Source/Timer.c
@@ -1,13 +1,20 @@
 #include "stm32f10x.h"
 #include "Timer.h"
 #include "GPIO.h"
+#define BROCHE_LED (5)
+#define OUT_PUSHPULL_2MHZ (0x2)
 
 void MyTimer_Base_Init ( MyTimer_Struct_TypeDef * Timer ) {
 	Timer->Timer->ARR = Timer->ARR;
 	Timer->Timer->PSC = Timer->PSC;
 	Timer->Timer->CR1 |= TIM_CR1_CEN;
+	Timer->Timer->DIER = TIM_DIER_UIE; // Vers pending
+	NVIC->ISER[0] |= NVIC_ISER_SETENA_30;
 }
 
+
+
 void TIM4_IRQHandler(void) {
-	
+	MyGPIO_Toggle(GPIOA, BROCHE_LED);
+	TIM4->SR &= ~(TIM_SR_UIF);
 }
--- Projet_2/Source/main.c
+++ Projet_2/Source/main.c
@@ -2,17 +2,18 @@
 #include "Timer.h"
 #include "GPIO.h"
 #define TIM4_ENABLE 2
+#define BROCHE_LED 5
 #define CEN_ENABLE 8
 
 int main ( void )
 {
 	MyTimer_Struct_TypeDef * MyTimer = &(MyTimer_Struct_TypeDef) {.Timer = TIM4, .ARR = MON_ARR, .PSC = MON_PSC};
+	MyGPIO_Struct_TypeDef* MyGPIO = &(MyGPIO_Struct_TypeDef) {.GPIO = GPIOA, .GPIO_Pin = BROCHE_LED, .GPIO_Conf = Out_Ppull};
+	
 	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; // Allumer TIM4
+	RCC->APB2ENR |= (0x01 << 2)|(0x01 << 3)|(0x01 << 4);
 	
-	TIM4->DIER = TIM_DIER_UIE; // Vers pending
-	
-	//|= NVIC_SetPendingIRQ
-	NVIC->ISER[0] |= NVIC_ISER_SETENA_30;
+	MyGPIO_Init(MyGPIO);
 	MyTimer_Base_Init(MyTimer);
 	
 	do
